746247632cfe7a9ebe4d3bfbace12a15aa88dee0
effaroukk <efqay1@gmail.com>
2024-10-14 23:34:54 +0000

    hard

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-'''Task 0's module.'''
+'''Task 0's module.
+'''
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
-    '''Waits for a random number of seconds between 0 and max_delay.'''
-    wait_time = random.uniform(0, max_delay)  # Ensure float in full range [0, max_delay]
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
     return wait_time
-
-# Example of calling the coroutine (not required for function definition):
-# asyncio.run(wait_random())
-
